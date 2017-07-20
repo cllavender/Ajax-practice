4eeedea51183121f56eaee4d6a9b83cbfec0909e
Lin Chen <cl_fighting@163.com>
2017-07-20 13:07:25 +0000

    fake files of index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   
   
+  devise_for :users
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
   get "/jquery-3" => "pages#jquery_3"
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
   
-  root "pages#jquery_1"
+  resources :posts
+  
+  root "posts#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
